3296303836feb23f5b1af1ca86c4c01516a28427
MelanyMFM <mefrancom@unal.edu.co>
2025-10-15 14:52:07 -0500

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
-
+import "./footer.css"
 
 export function Footer(){
 
     return(
         <div>
-            <p>Footer</p>
+            <div className="footer-links">
+                <p>Preguntas Frecuentes</p>
+                <p>Sobre Nosotros</p>
+            </div>
+            <p className="footer-message">
+                Marca registrada y derechos de 2025 Crustáceo Cascarudo® Corporation. 
+                Todos los derechos reservados.
+            </p>
         </div>
     );
 }
