bd211f07436f0a1e4a370339991a23a3875ec8db
MelanyMFM <mefrancom@unal.edu.co>
2025-10-15 14:02:02 -0500

    adelanto main

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,22 @@
 import "./home.css"
 import { Header } from "../../components/Header/Header";
+import spongebob from "../../assets/spongebob.png"
+import { Link } from "react-router-dom";
 
 export function Home() {
 
 
 
-  return <div>
-    <Header/>
+return (
+    <div className="home-container">
+            <Header/>
+            <p className="home-title">THE KRUSTY KRAB</p>
 
-    </div>;
+            <div className="home-center">
+                    <Link to="/menu">MENU</Link>
+                    <img className="spongebob" src={spongebob} alt="Spongebob" style={{ width: "20rem" }}/>
+
+            </div>
+
+    </div>);
 }
